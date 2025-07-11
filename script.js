09d218f73d700238f928302eafdc3000c143b371
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-07-11 16:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 0;
+
+setInterval(() => {
+  angle += 2;
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20);
